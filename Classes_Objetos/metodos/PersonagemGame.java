c3e2ecb4fa2f39a569fbe4c2b18302cc6b25aa13
Vitor Souza <vitorandre6@gmail.com>
2025-09-22 18:45:09 +0000

    add metodos

---
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -0,0 +1,37 @@
+class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+        if(saudeAtual - quantidadeDeDano > 0) {
+            saudeAtual -= quantidadeDeDano;
+        } else {
+            saudeAtual = 0;
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if(saudeAtual + quantidadeDeCura < 100) {
+            saudeAtual -= quantidadeDeCura;
+         } else {
+            saudeAtual = 100;
+         }
+    }
+}
