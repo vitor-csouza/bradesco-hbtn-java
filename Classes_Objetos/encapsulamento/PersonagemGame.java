c9b456d44eca52642ba90ad49a164d4044f6697a
Vitor Souza <vitorandre6@gmail.com>
2025-09-22 18:37:00 +0000

    add encapsulamento

---
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -0,0 +1,21 @@
+class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
