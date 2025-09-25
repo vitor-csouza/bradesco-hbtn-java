5cb9f11bb1825a3b4ebbe5eb8acdf773c9bb3a31
Vitor Souza <vitorandre6@gmail.com>
2025-09-25 19:38:30 +0000

    add heranca pedido livraria

---
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,18 @@
+public class Pedido {
+
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.itens = itens;
+    }
+
+    public double calcularTotal(){
+        double soma = 0;
+        for(ItemPedido item : itens) {
+            soma += item.getQuantidade() * item.getProduto().obterPrecoLiquido();
+        }
+        return soma;
+    }
+    
+}
