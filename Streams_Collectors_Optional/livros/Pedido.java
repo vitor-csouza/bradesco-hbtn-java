b15ad871122d6e57c8adffe3e98ab10141cac799
Vitor Souza <vitorandre6@gmail.com>
2025-10-17 16:38:03 +0000

    add stream

---
+++ Streams_Collectors_Optional/livros/Pedido.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class Pedido {
+
+    private int codigo;
+    private List<Produto> produtos;
+    private Cliente cliente;
+
+    public Pedido(int codigo, List<Produto> produtos, Cliente cliente) {
+        this.codigo = codigo;
+        this.produtos = produtos;
+        this.cliente = cliente;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Pedido [%d] - Cliente: %s - Itens: %d",
+                codigo,
+                cliente != null ? cliente.getNome() : "N/A",
+                produtos != null ? produtos.size() : 0);
+    }
+}
