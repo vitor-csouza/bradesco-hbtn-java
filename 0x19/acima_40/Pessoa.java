698a9ec9181decccc1d2d22e76d4fb8de193d56a
Vitor Souza <vitorandre6@gmail.com>
2025-10-21 12:49:35 +0000

    add collections

---
+++ 0x19/acima_40/Pessoa.java
@@ -0,0 +1,47 @@
+public class Pessoa {
+
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(new java.util.Locale("pt", "BR"),
+                "[%d] %s %s %d R$ %.2f",
+                codigo, nome, cargo, idade, salario);
+    }
+}
