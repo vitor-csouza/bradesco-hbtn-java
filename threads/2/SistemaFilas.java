1e95a9ca34362b8c72ad846008b5c768e0afdf58
Vitor Souza <vitorandre6@gmail.com>
2025-10-27 17:30:18 +0000

    add thread prsistemas filas

--- threads/2/SistemaFilas.java
+++ threads/2/SistemaFilas.java
@@ -1,10 +1,8 @@
 public class SistemaFilas {
     public static void main(String[] args) throws InterruptedException {
         Fila fila = new Fila(10);
-
         Thread p1 = new Produtor(fila, "Produtor-1");
         Thread p2 = new Produtor(fila, "Produtor-2");
-
         Thread c1 = new Consumidor(fila, "Consumidor-1");
         Thread c2 = new Consumidor(fila, "Consumidor-2");
 
@@ -13,7 +11,7 @@ public class SistemaFilas {
         c1.start();
         c2.start();
 
-        Thread.sleep(20_000);
+        Thread.sleep(20000);
 
         System.exit(0);
     }
