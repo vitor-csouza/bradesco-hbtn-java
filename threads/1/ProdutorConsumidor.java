3da2d12e56367e2da7a1909321db84ac4076c357
Vitor Souza <vitorandre6@gmail.com>
2025-10-27 17:14:44 +0000

    add thread produtos consumidor

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,19 @@
+public class ProdutorConsumidor {
+    public static void main(String[] args) throws InterruptedException {
+        Buffer buffer = new Buffer();
+
+        // Criar as threads
+        Thread produtor = new Produtor(buffer);
+        Thread consumidor = new Consumidor(buffer);
+
+        // Iniciar as threads
+        produtor.start();
+        consumidor.start();
+
+        // (opcional mas recomendado) Aguardar ambas terminarem
+        produtor.join();
+        consumidor.join();
+
+        System.out.println("Processo finalizado.");
+    }
+}
